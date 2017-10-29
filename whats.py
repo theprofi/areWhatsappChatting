051996b79ce094800afd24fbbb948795764f8584
Eli <eli1mail1@gmail.com>
2017-10-29 22:51:45 +0200

    Update whats.py

--- whats.py
+++ whats.py
@@ -1,9 +1,9 @@
 '''
  it's only the POC
- when the time comes I'll make the code cleaner
+Maybe later I'll make the code cleaner
 
  Requirements:
- * Download chromewebdriver for selenium and update the path of the dowbloaded executable in executable_path=
+ * Download chromewebdriver for selenium and update the path of the downloaded executable in executable_path=
  * Install the required libraries
 '''
 
